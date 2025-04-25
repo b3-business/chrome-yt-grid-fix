e3a29849d6d2a087386d1be7765ad6cda488105b
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-04-25 12:14:42 +0200

    fix async usage in background script

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -8,51 +8,58 @@ import { mainLogger } from "@src/utils/logger";
 const logger = mainLogger.getSubLogger({ name: "background" });
 logger.info("background script loaded");
 
-chrome.runtime.onMessage.addListener(
-  async (pureMessage, sender, sendResponse) => {
-    const messageParsed = ExtensionMessage.safeParse(pureMessage);
+chrome.runtime.onMessage.addListener((pureMessage, sender, sendResponse) => {
+  const messageParsed = ExtensionMessage.safeParse(pureMessage);
 
-    if (!messageParsed.success) {
-      logger.error(`DEV ERROR: Invalid message: ${messageParsed.error}`);
-      sendResponse({ error: `Invalid message: ${messageParsed.error}` });
-      return false;
-    }
-    const message = messageParsed.data;
-    logger.debug(`Received valid message: ${message}`);
+  if (!messageParsed.success) {
+    logger.error(`DEV ERROR: Invalid message: ${messageParsed.error.message}`);
+    sendResponse({
+      error: `Invalid message: ${messageParsed.error.message}`,
+    });
+    return false;
+  }
+  const message = messageParsed.data;
+  logger.debug(`Received valid message: `, message);
 
-    // Handle message: GET_GRID_ITEMS_PER_ROW
-    // used by the content script & the popup
-    // ------------------------------------------
-    if (message.type === "GET_GRID_ITEMS_PER_ROW") {
-      const response = await chrome.storage.sync.get(
-        "preferredGridItemsPerRow",
-      );
-      if (!response.preferredGridItemsPerRow) {
-        await chrome.storage.sync.set({ preferredGridItemsPerRow: 5 });
+  // Handle message: GET_GRID_ITEMS_PER_ROW
+  // used by the content script & the popup
+  // ------------------------------------------
+  if (message.type === "GET_GRID_ITEMS_PER_ROW") {
+    chrome.storage.sync
+      .get("preferredGridItemsPerRow")
+      .then((response) => {
+        logger.debug(`Raw sync storage response: `, response);
+
+        if (!response.preferredGridItemsPerRow) {
+          return chrome.storage.sync
+            .set({ preferredGridItemsPerRow: 5 })
+            .then(() => {
+              return response.preferredGridItemsPerRow;
+            });
+        }
+        return response.preferredGridItemsPerRow;
+      })
+      .then((preferredGridItemsPerRow) => {
         sendResponse({
           type: "GET_GRID_ITEMS_PER_ROW_RESPONSE",
-          value: 5,
+          value: preferredGridItemsPerRow,
         } satisfies GetGridItemsPerRowResponse);
-        return true;
-      }
-
-      sendResponse({
-        type: "GET_GRID_ITEMS_PER_ROW_RESPONSE",
-        value: response.preferredGridItemsPerRow,
-      } satisfies GetGridItemsPerRowResponse);
-      return true; // Indicates that the response will be sent asynchronously
-    }
-
-    // Handle message: UPDATE_GRID_ITEMS_PER_ROW
-    // ------------------------------------------
-    if (message.type === "UPDATE_GRID_ITEMS_PER_ROW") {
-      await chrome.storage.sync.set({
-        preferredGridItemsPerRow: message.value,
       });
-      sendResponse({
-        type: "UPDATE_GRID_ITEMS_PER_ROW_RESPONSE",
-      } satisfies UpdateGridItemsPerRowResponse);
-      return true;
-    }
-  },
-);
+    return true;
+  }
+
+  // Handle message: UPDATE_GRID_ITEMS_PER_ROW
+  // ------------------------------------------
+  if (message.type === "UPDATE_GRID_ITEMS_PER_ROW") {
+    chrome.storage.sync
+      .set({
+        preferredGridItemsPerRow: message.value,
+      })
+      .then(() => {
+        sendResponse({
+          type: "UPDATE_GRID_ITEMS_PER_ROW_RESPONSE",
+        } satisfies UpdateGridItemsPerRowResponse);
+      });
+    return true;
+  }
+});
