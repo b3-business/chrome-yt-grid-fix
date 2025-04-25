354676b88ca1eaef798454a642aba3dac1062332
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-04-25 10:57:05 +0200

    store the preferred grid size in chrome sync storage

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,22 +1,54 @@
-import logoDataUrl from "@src/assets/img/logo.svg";
 import "@src/styles/tailwind.css";
+import { createResource } from "solid-js";
+import { popupLogger } from "./popupLogger";
 
 export function PopupPage() {
+  const [preferredGridItemsPerRow, { mutate, refetch }] = createResource(
+    async () => {
+      // Send message to background script to get the value
+      // const response = await chrome.runtime.sendMessage({
+      //   type: "GET_GRID_ITEMS_PER_ROW",
+      // });
+      const response = await chrome.storage.sync.get(
+        "preferredGridItemsPerRow",
+      );
+
+      if (!response.preferredGridItemsPerRow) {
+        chrome.storage.sync.set({ preferredGridItemsPerRow: 5 });
+        return 5;
+      }
+      popupLogger.debug(
+        `Got preferred grid items per row: ${response.preferredGridItemsPerRow}`,
+      );
+      return response.preferredGridItemsPerRow;
+    },
+  );
+
+  const handleChange = (e: Event) => {
+    const target = e.target as HTMLInputElement;
+    const value = parseInt(target.value, 10);
+
+    chrome.storage.sync.set({ preferredGridItemsPerRow: value }, () => {
+      popupLogger.debug(`Set preferred grid items per row: ${value}`);
+      mutate(value);
+    });
+  };
+
   return (
-    <div class="h-[200px] w-[400px] bg-[#282c34] text-white">
-      <h1 class="text-xl font-bold">Popup Page Template</h1>
-      <section class="w-fit p-5">
-        {/* get urls for imported assets: chrome.runtime.getURL(), but NOT for images, images are inlined as data urls from vite 5 on! */}
-        <img
-          src={logoDataUrl}
-          class="motion-preset-spin motion-duration-[2s] pointer-events-none"
-          alt="logo"
+    <div class="h-[200px] w-[400px] bg-neutral-900 p-4 text-base text-white">
+      <h1 class="mb-4 text-xl font-bold">YouTube Grid Fix</h1>
+      <div class="flex items-center">
+        <label for="grid-items-per-row" class="mr-2 w-max min-w-max text-base">
+          Preferred grid items per row:
+        </label>
+        <input
+          class="w-full appearance-none rounded-md border border-neutral-700 bg-neutral-800 p-2 text-white focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:outline-none"
+          id="grid-items-per-row"
+          type="number"
+          value={preferredGridItemsPerRow()}
+          onchange={handleChange}
         />
-        <p class="flex flex-wrap pt-10 text-base font-bold">
-          Edit &nbsp; <code>src/pages/options/Options.tsx</code> &nbsp; and save
-          to reload.
-        </p>
-      </section>
+      </div>
     </div>
   );
 }
