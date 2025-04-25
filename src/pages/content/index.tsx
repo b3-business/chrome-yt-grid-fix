fc0760c7c1406553105009135c5ce33075574771
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-04-25 11:36:17 +0200

    use new messages in content script

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,26 +1,69 @@
+import {
+  ExtensionMessage,
+  GetGridItemsPerRowMessage,
+  GetGridItemsPerRowResponse,
+} from "@src/lib/ExtensionMessage.type";
 import { contentLogger } from "./contentLogger";
 import { waitOnceForElement } from "./waitOnceForElement";
 
+const richGridSelector = "#contents.ytd-rich-grid-renderer";
+let richGridElement: HTMLElement | undefined;
+
 async function main() {
-  const richGridRenderer$ = await waitOnceForElement(
-    "#contents.ytd-rich-grid-renderer",
-  );
-  const preferredGridItemsPerRow = await chrome.runtime.sendMessage({
+  richGridElement = await waitOnceForElement(richGridSelector);
+  const preferredGridItemsPerRow = await chrome.runtime.sendMessage<
+    GetGridItemsPerRowMessage,
+    GetGridItemsPerRowResponse
+  >({
     type: "GET_GRID_ITEMS_PER_ROW",
   });
-  const style = window.getComputedStyle(richGridRenderer$);
+
+  updateGridItemsPerRow(richGridElement, preferredGridItemsPerRow.value);
+}
+main();
+
+chrome.runtime.onMessage.addListener(
+  (originalMessage, sender, sendResponse) => {
+    const parsedMessage = ExtensionMessage.safeParse(originalMessage);
+    if (!parsedMessage.success) {
+      contentLogger.error(`Invalid message: ${parsedMessage.error}`);
+      sendResponse({ error: `Invalid message: ${parsedMessage.error}` });
+      return false;
+    }
+    const message = parsedMessage.data;
+    contentLogger.debug(`Received message: ${message}`);
+
+    if (message.type === "UPDATE_GRID_ITEMS_PER_ROW") {
+      updateGridItemsPerRow(richGridElement, message.value);
+      return true;
+    }
+
+    return false;
+  },
+);
+
+function updateGridItemsPerRow(
+  richGridElement: HTMLElement | undefined,
+  preferredGridItemsPerRow: number,
+) {
+  if (!richGridElement) {
+    contentLogger.error(
+      "Rich grid element not passed to updateGridItemsPerRow()",
+    );
+    return;
+  }
+
+  const style = window.getComputedStyle(richGridElement);
   const gridItemsPerRow = style.getPropertyValue(
     "--ytd-rich-grid-items-per-row",
   );
-  contentLogger.info(`YT native grid items per row: ${gridItemsPerRow}`);
+  contentLogger.info(`Last grid items per row: ${gridItemsPerRow}`);
 
-  richGridRenderer$.style.setProperty(
+  richGridElement.style.setProperty(
     "--ytd-rich-grid-items-per-row",
-    preferredGridItemsPerRow,
+    preferredGridItemsPerRow.toString(),
   );
   contentLogger.info(
     `Overwrote grid items per row to: ${preferredGridItemsPerRow}`,
   );
 }
-
-main();
